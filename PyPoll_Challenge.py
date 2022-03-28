b8dfebb646ec6e680f794aec4703133a5a263772
Tifarahani <101008318+Tifarahani@users.noreply.github.com>
2022-03-27 20:31:13 -0400

    Add files via upload

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -7,9 +7,9 @@ import os
 from pydoc import describe
 
 # Add a variable to load a file from a path.
-file_to_load = os.path.join(".." ,"Analysis", "election_results.csv")
+file_to_load = os.path.join(".." ,"Resources", "election_results.csv")
 # Add a variable to save the file to a path.
-file_to_save = os.path.join("..","Analysis", "election_analysis.txt")
+file_to_save = os.path.join("..","Resources", "election_analysis.txt")
 
 
 # Initialize a total vote counter.
